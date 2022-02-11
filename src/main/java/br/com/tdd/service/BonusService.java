f072c97f90ed9a78d9f30fb0cff63f935fd6c376
geovanis <geovanis@ciandt.com>
2022-02-11 15:53:47 -0300

    service implemented ready to test

---
+++ src/main/java/br/com/tdd/service/BonusService.java
@@ -0,0 +1,18 @@
+package br.com.tdd.service;
+
+import br.com.tdd.modelo.Funcionario;
+
+import java.math.BigDecimal;
+
+public class BonusService {
+
+    public BigDecimal calcularBonus(Funcionario funcionario){
+        BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+
+        if (valor.compareTo(new BigDecimal("1000")) > 0){
+            valor = BigDecimal.ZERO;
+        }
+
+        return valor;
+    }
+}
