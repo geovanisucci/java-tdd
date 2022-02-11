6910760e35a491c6e9f6a13454eb975cf4184673
geovanis <geovanis@ciandt.com>
2022-02-11 13:33:43 -0300

    test structure

---
+++ src/test/java/br/com/tdd/tests/CalculadoraTestes.java
@@ -0,0 +1,22 @@
+package br.com.tdd.tests;
+
+import br.com.tdd.Calculadora;
+
+public class CalculadoraTestes {
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int soma;
+
+        soma = calculadora.somar(3,7);
+        System.out.println(soma);
+
+
+        soma = calculadora.somar(3,0);
+        System.out.println(soma);
+
+
+        soma = calculadora.somar(0,0);
+        System.out.println(soma);
+    }
+}
